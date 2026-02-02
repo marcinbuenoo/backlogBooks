3291f0453b2638eeaf461ff7b086dc3b5c0699f8
MarcioBuenoo <marcinbueno01@hotmail.com>
2026-02-02 20:10:06 -0300

    feat: working on a side panel for UI/UX

---
+++ front/src/components/SidePanel.tsx
@@ -0,0 +1,14 @@
+import "../styles/SidePanel.css"
+
+export default function SidePanel() {
+    return (
+        <div className="panel-container">
+            <div className="panel-button-lits">
+
+            </div>
+            <div className="panel-footer">
+
+            </div>
+        </div>
+    )
+}
